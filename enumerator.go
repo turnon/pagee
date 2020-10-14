22093a021c1ea88a0a804bfe217ad7ccdcffaaf0
ken <block24block@gmail.com>
2020-10-15 00:20:16 +0800

    FinRange

--- enumerator.go
+++ enumerator.go
@@ -1,15 +1,28 @@
 package pagee
 
-func newEnum(from, to int) <-chan int {
+func newFinRange(from, to int, step ...int) <-chan int {
+	st := 1
+	if len(step) >= 1 {
+		st = step[0]
+	}
+
+	ex := func(fr int) bool {
+		return fr > to
+	}
+
+	return newRange(from, st, ex)
+}
+
+func newRange(from int, step int, exceeded func(int) bool) <-chan int {
 	ch := make(chan int)
 	go func() {
 		for {
-			ch <- from
-			if from >= to {
+			if exceeded(from) {
 				close(ch)
 				return
 			}
-			from++
+			ch <- from
+			from += step
 		}
 	}()
 	return ch
--- enumerator_test.go
+++ enumerator_test.go
@@ -2,15 +2,26 @@ package pagee
 
 import "testing"
 
-func TestNewEnum(t *testing.T) {
-	enum := newEnum(1, 3)
+func TestNewFinRangeWithoutStep(t *testing.T) {
+	enum := newFinRange(1, 3)
 	arr := []int{}
 	for n := range enum {
 		arr = append(arr, n)
 	}
 
-	errMsg := "newEnum is wrong"
-	if arr[0] != 1 || arr[1] != 2 || arr[2] != 3 || len(arr) != 3 {
-		t.Error(errMsg, arr)
+	if len(arr) != 3 || arr[0] != 1 || arr[1] != 2 || arr[2] != 3 {
+		t.Error(arr)
+	}
+}
+
+func TestNewFinRangeWithStep(t *testing.T) {
+	enum := newFinRange(1, 6, 2)
+	arr := []int{}
+	for n := range enum {
+		arr = append(arr, n)
+	}
+
+	if len(arr) != 3 || arr[0] != 1 || arr[1] != 3 || arr[2] != 5 {
+		t.Error(arr)
 	}
 }
